49c88367d732577d1c8885a037e26bbd0dfdf1c9
martin <martin@yotellevocuba.com>
2020-05-19 21:59:25 -0400

    merge con master

--- scripts/2020-05-04 Created table drivers_unapproved.sql
+++ scripts/2020-05-04 Created table drivers_unapproved.sql
@@ -1,4 +1,19 @@
-CREATE TABLE `drivers_unapproved` ( `id` SERIAL NOT NULL , `username` TEXT NOT NULL , `full_name` TEXT NOT NULL , `province_id` BIGINT UNSIGNED NOT NULL , `car_model` TEXT NOT NULL , `max_people_count` INT NOT NULL , `has_classic_car` BOOLEAN NOT NULL , `has_air_conditioner` BOOLEAN NOT NULL , `speaks_english` BOOLEAN NOT NULL , `has_modern_car` BOOLEAN NOT NULL , `about` TEXT NOT NULL , `featured_img_url` TEXT NOT NULL , `avatar_path` TEXT NOT NULL , `profile_img_url` TEXT NOT NULL ) ENGINE = InnoDB;
+CREATE TABLE `drivers_unapproved` ( 
+`id` SERIAL NOT NULL , 
+`username` TEXT NOT NULL , 
+`full_name` TEXT NOT NULL , 
+`province_id` BIGINT UNSIGNED NOT NULL , 
+`car_model` TEXT NOT NULL , 
+`max_people_count` INT NOT NULL , 
+`has_classic_car` BOOLEAN NOT NULL , 
+`has_air_conditioner` BOOLEAN NOT NULL , 
+`speaks_english` BOOLEAN NOT NULL , 
+`has_modern_car` BOOLEAN NOT NULL , 
+`about` TEXT NOT NULL , 
+`featured_img_url` TEXT NOT NULL , 
+`avatar_path` TEXT NOT NULL , 
+`profile_img_url` TEXT NOT NULL ) 
+ENGINE = InnoDB;
 
 ALTER TABLE `drivers_unapproved` ADD `min_people_count` INT NOT NULL AFTER `car_model`;
 
